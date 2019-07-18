503ed02e60cdef51b1270c1dfac5c1221e9a1354
vincentnqc <vincentnqc92@gmail.com>
2019-07-17 19:37:53 -0500

    java

---
+++ code-along/java/app.js
@@ -0,0 +1,6 @@
+var myName = "Vincent Quitta-Carney"
+
+console.log(myName)
+console.log('myName:', myName)
+var nameElement = document.getElementsByClassName('name')[0]
+nameElement.innerHTML = myName

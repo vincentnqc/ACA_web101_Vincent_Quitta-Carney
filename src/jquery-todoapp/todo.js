b023cb59b7f649ee55dab23f0615e691802efdfc
vincentnqc <vincentnqc92@gmail.com>
2019-07-23 17:00:10 -0500

    push

--- src/jquery-todoapp/todo.js
+++ src/jquery-todoapp/todo.js
@@ -6,8 +6,16 @@ $(function() {
   $newItemForm.on('submit', function(e) {
     e.preventDefault();
     let text = $('input[type="text"]').val();
-    $list.append(`<li>${text}</li>`);
-    $('input[type="text"]').val('');
+    if(text=="")
+    {
+      alert("please enter text");
+    }
+    else
+  {$list.append(`<li>${text}</li>`);
+  $('input[type="text"]').val('');
+
+  }
+    
   });
 
   $list.on('click', 'li', function() {

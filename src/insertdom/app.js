ea0d1b102aef1b3b0b1220aecdbd1b4f80a81890
vincentnqc <vincentnqc92@gmail.com>
2019-07-29 19:42:57 -0500

    better

--- src/insertdom/app.js
+++ src/insertdom/app.js
@@ -3,7 +3,7 @@ const projectList = [
     name: 'project1',
     desc: 'a lil description',
     url: 'https://picsum.photos/200?random=1',
-    tags: '#css, #html, #bootstrap',
+    tags: '#css, #html, #bootstrap'
 
   },
 
@@ -11,35 +11,39 @@ const projectList = [
 name: 'project1',
   desc: 'a lil description',
   url: 'https://picsum.photos/200?random=1',
-  tags: '#css, #html, #bootstrap',
+  tags: '#css, #html, #bootstrap'
 
 },
+
 {
 name: 'project1',
   desc: 'a lil description',
-    url: 'https://picsum.photos/200?random=1',
-      tags: '#css, #html, #bootstrap',
+    url: 'https://picsum.photos/200?random=2',
+      tags: '#css, #html, #bootstrap'
 
 },
+
 {
 name: 'project1',
   desc: 'a lil description',
-    url: 'https://picsum.photos/200?random=1',
-      tags: '#css, #html, #bootstrap',
+    url: 'https://picsum.photos/200?random=3',
+      tags: '#css, #html, #bootstrap'
 
 },
+
 {
 name: 'project1',
   desc: 'a lil description',
-    url: 'https://picsum.photos/200?random=1',
-      tags: '#css, #html, #bootstrap',
+    url: 'https://picsum.photos/200?random=4',
+      tags: '#css, #html, #bootstrap'
 
 },
+
 {
 name: 'project1',
   desc: 'a lil description',
-    url: 'https://picsum.photos/200?random=1',
-      tags: '#css, #html, #bootstrap',
+    url: 'https://picsum.photos/200?random=5',
+      tags: '#css, #html, #bootstrap'
 
 },
 ]
@@ -47,15 +51,15 @@ name: 'project1',
 let rowDiv = document.querySelector('.row')
 console.log('rowDiv', rowDiv)
 
-projectList.forEach(element => {
+projectList.forEach(project => {
   // console.log('element, element')
   let cardTemplate = `<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${ project.url }" alt="Card image cap">
+    <img class="card-img-top" src= "${ project.url }" alt="Card image cap">
       <div class="card-body">
-        <h5 class="card-title">$ {project.name }</h5>
-        <p class="card-text">$ {project.desc }</p>
+        <h5 class="card-title">${project.name }</h5>
+        <p class="card-text">${project.desc }</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
-</div > `
-  row.Div.insertAdjacetHTML('beforeend', cardTemplate)
+</div>`
+  rowDiv.insertAdjacentHTML('beforeend', cardTemplate)
 });
